6bd94c9908f7ac38559d495dc527b54fdc298736
willJOIN <whirano4@gmail.com>
2023-09-08 19:46:33 -0300

    feat: basic front

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.post('/api-ccb/word', (req, res) => {
+app.post('/api-ccb/word', async (req, res) => {
     const { book, chapter } = req.body;
     const url = `https://www.abibliadigital.com.br/api/verses/acf/${book}/${chapter}`;
+    let responseStatus = 500; 
 
-    axios.get(url)
-        .then(response => {
-            const hymn1 = getHymn(1, 480)
-            const hymn2 = getHymn(1, 480)
-            const hymn3 = getHymn(1, 480)
-            const finalHymn = getHymn(1, 480)
+    while (responseStatus === 500) {
+        try {
+            const response = await axios.get(url);
+            responseStatus = response.status; 
 
-            const word = getWord(response.data, hymn1, hymn2, hymn3, finalHymn)
+            if (responseStatus === 200) {
+                const hymn1 = getHymn(1, 480);
+                const hymn2 = getHymn(1, 480);
+                const hymn3 = getHymn(1, 480);
+                const finalHymn = getHymn(1, 480);
 
-            res.status(200).json(word);
-        })
-        .catch(error => {
+                const word = getWord(response.data, hymn1, hymn2, hymn3, finalHymn);
+
+                return res.status(200).json(word); 
+            }
+        } catch (error) {
             console.error('Error:', error);
-            console.error('Request URL:', url); 
-            res.status(500).json({ error: 'Internal server error' });
-        });
+            console.error('Request URL:', url);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+
+    return res.status(500).json({ error: 'Internal server error' }); 
 });
 
 function getHymn(min, max) { 
@@ -36,34 +46,33 @@ function getWord(data, hymn1, hymn2, hymn3, finalHymn) {
     const chapterInfo = data.chapter;
     const verses = data.verses;
 
-    let word = ''; // Inicialize 'word' como uma string vazia
+    let word = ''; 
 
     for (const verse of verses) {
         word += verse.text + ' ';
     }
 
     word = {
+        "primeiro hino": hymn1,
+        "segundo hino": hymn2,
+        "terceiro hino": hymn3,
+        "oracao": getOurFather(),
         palavra: {
-            "primeiro hino": hymn1,
-            "segundo hino": hymn2,
-            "terceiro hino": hymn3,
             livro: bookInfo.name,
             capitulo: chapterInfo.number,
             versos: word.trim(),
-            "oracao": getOurFather(),
-            "hino de encerramento": finalHymn
-        }
+        },   
+        "hino de encerramento": finalHymn
     };
 
     return word;
 }
 
-
 function getOurFather() {
     return `Pai nosso, que estás nos céus, santificado seja o teu nome. Venha o teu reino, seja feita a tua vontade, assim na terra como no céu. O pão nosso de cada dia nos dá hoje. E perdoa-nos as nossas dívidas, assim como nós perdoamos aos nossos devedores. E não nos conduzas à tentação; mas livra-nos do mal; porque teu é o reino, e o poder, e a glória, para sempre. Amém.`;
 }
 
-const port = 3333;
+const port = 5500;
 
 app.listen(port, () => {
     console.log("Server is active on port", port);
